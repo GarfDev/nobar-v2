03fd07e3a3f0eba39aa7fd1b91ef2fd624b9378c
GarfDev <garfdev.13@gmail.com>
2023-10-09 20:29:20 +0700

    fix: correct language setting

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,53 @@
-import { NextRequest, NextResponse } from "next/server";
-import acceptLanguage from "accept-language";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const fallbackLng = "vi-VN";
-const languages = ["en-US", "vi-VN"];
-const cookieName = "NEXT_LOCALE";
+// Regex to check whether something has an extension, e.g. .jpg
+const PUBLIC_FILE = /\.(.*)$/;
+const FALLBACK_LANG = "vi";
 
-acceptLanguage.languages(languages);
+export function middleware(request: NextRequest) {
+  const { nextUrl, headers, cookies } = request;
+  // Cloned url to work with
+  const url = nextUrl.clone();
+  // Client language, defaults to en
+  const language =
+    cookies.get("NEXT_LOCALE")?.value ||
+    headers
+      .get("accept-language")
+      ?.split(",")?.[0]
+      .split("-")?.[0]
+      .toLowerCase() ||
+    FALLBACK_LANG;
 
-export const config = {
-  // matcher: '/:lng*'
-  matcher: [/\.(.*)$/],
-};
+  try {
+    // Early return if it is a public file such as an image or an api call
+    if (
+      PUBLIC_FILE.test(nextUrl.pathname) ||
+      nextUrl.pathname.includes("/api")
+    ) {
+      return undefined;
+    }
 
-export function middleware(req: NextRequest) {
-  let lng;
-  if (req.cookies.has(cookieName))
-    lng = acceptLanguage.get(req.cookies.get(cookieName)!.value);
-  if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
-  if (!lng) lng = fallbackLng;
+    // Proceed without redirection if on a localized path
+    if (
+      nextUrl.pathname.startsWith("/en") ||
+      nextUrl.pathname.startsWith("/vi")
+    ) {
+      return undefined;
+    }
 
-  // Redirect if lng in path is not supported
-  if (
-    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !req.nextUrl.pathname.startsWith("/_next")
-  ) {
-    return NextResponse.redirect(
-      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
-    );
+    if (language === "en") {
+      url.pathname = `/en${nextUrl.pathname}`;
+      return NextResponse.redirect(url);
+    }
+
+    if (language === "vi") {
+      url.pathname = `/vi${nextUrl.pathname}`;
+      return NextResponse.redirect(url);
+    }
+
+    return undefined;
+  } catch (error) {
+    console.log(error);
   }
-
-  if (req.headers.has("referer")) {
-    const refererUrl = new URL(req.headers.get("referer")!);
-    const lngInReferer = languages.find((l) =>
-      refererUrl.pathname.startsWith(`/${l}`)
-    );
-    const response = NextResponse.next();
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
-    return response;
-  }
-
-  return NextResponse.next();
 }
